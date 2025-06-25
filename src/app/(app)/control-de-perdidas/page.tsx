33cc5ac46e9e0047fedbb0b7b2af4ed9d807538a
Omar Oswaldo Sanchez <omar.sanchezfigueroa@mercadolibre.com.mx>
2025-06-25 23:01:26 +0000

    Algo paso que ya no abre la sección de control de perdidas

--- src/app/(app)/control-de-perdidas/page.tsx
+++ src/app/(app)/control-de-perdidas/page.tsx
@@ -80,7 +80,7 @@ function RepsEnTurno() {
           <ul className="space-y-4">
             {onDutyReps.map((rep) => (
               <li key={rep.usuario} className="flex items-center space-x-4">
-                <Avatar className="h-12 w-12 border-2 border-green-500">
+                <Avatar className="h-12 w-12 border-2 border-primary">
                    <AvatarImage src={`https://avatar.vercel.sh/${rep.usuario.replace(/\s/g, '')}.png?s=100`} alt={rep.usuario} />
                   <AvatarFallback>{getInitials(rep.usuario)}</AvatarFallback>
                 </Avatar>
@@ -107,7 +107,7 @@ function RepsEnTurno() {
 export default function ControlDePerdidasPage() {
   const lookerStudioEmbedUrl = "https://lookerstudio.google.com/embed/reporting/2a1228bc-36fb-4f28-8190-38a5149ee670/page/p_t5v3dj3pnd";
   const chatUrl = "https://mail.google.com/chat/u/0/#chat/dm/00i2ncAAAAE";
-  const imageUrl = "https://lh3.googleusercontent.com/a-/ALV-UjW2KHH26Xf-ouY0wtc2rkXunbgvjlqFaJRTt4x2GpMNiAtMkuPLx1OWuMoe_nCEyahd8YOyqNK1rWC5YCZ1tulhmrnk8Tpju5AgQoXyQEP7EvShPq9pIdYrVbSQU29NwlZgqS6VbYdTnGFv7UG3prhJRYiJVZM3DbwVY97k6oKgKjAe9UbZv4REmBWXNeYrdA7JsJH4zu4U47WDD0BUYEhKk5h4bvqngSOEPtJj_nvL3ToCOKN88jRbSscBpwrayafgi2SGFftfW3xro01vs0kbOx4QrlyeujhOFhtSCfsHa7wJP2isKod-J8SU3sdcDkSTbZTZ1ohC46sVAwIxYOoISzBeVKPNRGNRO89GGRvZS8g10GfuE8eBRj2vEGX66hAQST_JZVu0pDaRzC2nKeKA3g8t2V4Ib9BRZ46GbJK8tXGG-3b_Ibmmgra2SyZCzh_jou3jbAJ0c1Sm32Fmi3ttsuoXab5P665F8qTRVdqOivBPbxAa6CUm-DJLOO6w9VsxN6iG55xAWGmZWvxcz1pcBtBCWVucXanRSRTFaRhcPpBrMougQpgguacwk0Pg0usjfQ-W3-nXEV4gNdAaOZSyj6jV2aL-J_mAwXRKvjtaKJP8gy9MPpUlyCeKdhtzfzpxBpnxyp4CZrQKBEcyXnUBiUbxUaNFwAaH_MHO1sB3nCQSvS9jjXA3htDPMZAQnK5J_zOBIUap3oTZL51nu3MGKlIuN_5uAZpzPPgOpbReICq-h7sdea_RBvmuBHmWwmy1iFiKqxrr2M8Gp50QH_RrCp2wUtuGYIl1er7fz9tY19dPJeqKmxqmMFmqAFp07HPk21c9tRNr8pvyhPPOyLnpvFvitxU7u5ZyAc6VsK6l4F46V88HU5aVX8XIdT6vLhvMudc1UyuACeWaKZWJkML_SRfg40IJY7kmWq0nGJ5WTyXp5KLl0PJJgIz4FKhXC3_uKHIc2slakfstTNmGJBdBTZqrF9Q83A=s272-p-k-rw-no";
+  const imageUrl = "https://lh3.googleusercontent.com/a-/ALV-UjW2KHH26Xf-ouY0wtc2rkXunbgvjlqFaJRTt4x2GpMNiAtMkuPLx1OWuMoe_nCEyahd8YOyqNK1rWC5YCZ1tulhmrnk8Tpju5AgQoXyQEP7EvShPq9pIdYrVbSQU29NwlZgqS6VbYdTnGFv7UG3prhJRYiJVZM3DbwVY97k6oKgKjAe9UbZv4REmBWXNeYrdA7JsJH4zu4U47WDD0BUYEhKk5h4bvqngSOEPtJj_nvL3ToCOKN88jRbSscBpwrayafgi2SGFftfW3xro01vs0kbOx4QrlyeujhOFhtSCfsHa7wJP2isKod-J8SU3sdcDkSTbZTZLohC46sVAwIxYOoISzBeVKPNRGNRO89GGRvZS8g10GfuE8eBRj2vEGX66hAQST_JZVu0pDaRzC2nKeKA3g8t2V4Ib9BRZ46GbJK8tXGG-3b_Ibmmgra2SyZCzh_jou3jbAJ0c1Sm32Fmi3ttsuoXab5P665F8qTRVdqOivBPbxAa6CUm-DJLOO6w9VsxN6iG55xAWGmZWvxcz1pcBtBCWVucXanRSRTFaRhcPpBrMougQpgguacwk0Pg0usjfQ-W3-nXEV4gNdAaOZSyj6jV2aL-J_mAwXRKvjtaKJP8gy9MPpUlyCeKdhtzfzpxBpnxyp4CZrQKBEcyXnUBiUbxUaNFwAaH_MHO1sB3nCQSvS9jjXA3htDPMZAQnK5J_zOBIUap3oTZL51nu3MGKlIuN_5uAZpzPPgOpbReICq-h7sdea_RBvmuBHmWwmy1iFiKqxrr2M8Gp50QH_RrCp2wUtuGYIl1er7fz9tY19dPJeqKmxqmMFmqAFp07HPk21c9tRNr8pvyhPPOyLnpvFvitxU7u5ZyAc6VsK6l4F46V88HU5aVX8XIdT6vLhvMudc1UyuACeWaKZWJkML_SRfg40IJY7kmWq0nGJ5WTyXp5KLl0PJJgIz4FKhXC3_uKHIc2slakfstTNmGJBdBTZqrF9Q83A=s272-p-k-rw-no";
 
   return (
     <Card className="shadow-lg">
