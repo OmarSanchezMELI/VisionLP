2a21578cd2aa63472dafe094f2ed89fa6556d112
Omar Oswaldo Sanchez <omar.sanchezfigueroa@mercadolibre.com.mx>
2025-06-25 22:39:05 +0000

    Ahora quiero que le des acceso a la aplicación a los siguientes correos:

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,10 @@
 
 export const authorizedEmails: string[] = [
   'omar.sanchezfigueroa@mercadolibre.com.mx',
+  'daniel.mejiaibarra@mercadolibre.com.mx',
+  'omar.garciavaldez@mercadolibre.com.mx',
+  'alexis.tolentinorivera@mercadolibre.com.mx',
+  'isidro.contreras@mercadolibre.com.mx',
   // For testing purposes, you might add other emails here temporarily
   // e.g., 'test@example.com'
 ];
